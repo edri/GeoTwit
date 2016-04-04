a35d09aa9fb8bce9de52d49cb13b1310c58bb3cc
edri <mig.santamar@gmail.com>
2016-04-04 16:58:42 +0200

    Update Twitter4J test-application and update documentation.

---
+++ tests/twitter4jDesktop/src/twitter4jDesktop/StreamingStatsThread.java
@@ -0,0 +1,61 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package twitter4jDesktop;
+
+import java.util.Observable;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ *
+ * @author miguel
+ */
+public class StreamingStatsThread extends Observable implements Runnable {
+   private final String queryString;
+   private final long timeBetweenStats;
+   private final MainExecution mainExecution;
+   private final Thread thread;
+   
+   public StreamingStatsThread(String queryString, long timeBetweenStats, MainExecution mainExecution) {
+      this.queryString = queryString;
+      this.timeBetweenStats = timeBetweenStats;
+      this.mainExecution = mainExecution;      
+      thread = new Thread(this);
+      thread.start();
+   }
+   
+   @Override
+   public void run() {
+      long nbReceivedTweets, nbTweetsWithGeoloc, nbTweetsWithRightGeoloc;
+      double percentsGeoloc, percentRightGeoloc;
+      
+      while (true) {
+         try {
+            Thread.sleep(timeBetweenStats);
+            // Get Streaming's stats data.
+            nbReceivedTweets = mainExecution.getNbReveivedTweets();
+            nbTweetsWithGeoloc = mainExecution.getNbTweetsWithLocation();
+            nbTweetsWithRightGeoloc = mainExecution.getNbTweetsWithRightLocation();
+            // Then reset data for the next analysis.
+            mainExecution.resetData();
+            
+            // Calculate and display stats.
+            percentsGeoloc = Math.round(nbTweetsWithGeoloc * (100.0 / nbReceivedTweets) * 100) / (double)100;
+            percentRightGeoloc = Math.round(nbTweetsWithRightGeoloc * (100.0 / nbReceivedTweets) * 100) / (double)100;
+            System.out.println("I received " + nbReceivedTweets + " Tweets in " + 
+                               timeBetweenStats / 1000 + " seconds" + ", including " + 
+                               nbTweetsWithGeoloc + " ones WITH geolocalion tags and " + 
+                               nbTweetsWithRightGeoloc + " ones with the wanted geolocation.");
+            System.out.println("This means " + percentsGeoloc + " % of the received " + 
+                               "Tweets with the \"" + queryString + "\" tag(s) owned a" +
+                               " geolocation tag and " + percentRightGeoloc + " % " + 
+                               "contained the desired location.");
+         } catch (InterruptedException ex) {
+            Logger.getLogger(StreamingStatsThread.class.getName()).log(Level.SEVERE, null, ex);
+         }
+      }
+   }
+}
--- tests/twitter4jDesktop/src/twitter4jDesktop/Twitter4j.java
+++ tests/twitter4jDesktop/src/twitter4jDesktop/Twitter4j.java
@@ -8,162 +8,58 @@ package twitter4jDesktop;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import twitter4j.FilterQuery;
-import twitter4j.GeoLocation;
-import twitter4j.Query;
-import twitter4j.QueryResult;
-import twitter4j.StallWarning;
-import twitter4j.Status;
-import twitter4j.StatusDeletionNotice;
-import twitter4j.StatusListener;
-import twitter4j.Twitter;
+import java.util.concurrent.ExecutionException;
 import twitter4j.TwitterException;
-import twitter4j.TwitterFactory;
-import twitter4j.TwitterStream;
-import twitter4j.TwitterStreamFactory;
-import twitter4j.auth.AccessToken;
-import twitter4j.auth.RequestToken;
-import twitter4j.conf.Configuration;
-import twitter4j.conf.ConfigurationBuilder;
 
 /**
  *
  * @author miguel
  */
 public class Twitter4j {
-   public static final String CONSUMER_KEY = "zvPuOcouiYZTC1SX8oxxFUJlF";
-   public static final String CONSUMER_SECRET = "DtKSu11TQeqly23aJbpjNtGM2KtQoz71zBCPhLNt76I6daJOXV";
-   public static Twitter twitter = TwitterFactory.getSingleton();
-
    /**
-    * Get and return the application's access token, which will be used for the
-    * Twitter's APIs' queries.
-    * @return the access token
-    * @throws TwitterException
+    * Read user's input and return the Twitter's tags the user want to search.
+    * @return the Twitter's tags the user want to search
     * @throws IOException 
     */
-   public static AccessToken getApplicationAccessToken() throws TwitterException, IOException {
-      twitter.setOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET);
-      RequestToken requestToken = twitter.getOAuthRequestToken();
-      AccessToken accessToken = null;
+   public static String readTags() throws IOException {
       BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+      boolean success = false;
+      String filter = "";
       
-      while (accessToken == null) {
-         System.out.println("Open the following URL and grant access to your account:");
-         System.out.println(requestToken.getAuthorizationURL());
-         System.out.print("Enter the PIN (if aviailable) or just hit enter. [PIN]: ");
-         String pin = br.readLine();
+      do {
+         System.out.print("Please enter some tags to search: ");
          
-         try {
-            if (pin.length() > 0) {
-               accessToken = twitter.getOAuthAccessToken(requestToken, pin);
-               System.out.println("Yay! Token successfully got: " + accessToken);
-            } else {
-               accessToken = twitter.getOAuthAccessToken();
-            }
-         } catch (TwitterException te) {
-            if (te.getStatusCode() == 401) {
-               System.out.println("Unable to get the access token.");
-            } else {
-               te.printStackTrace();
-            }
-         }
-      }
+         filter = br.readLine();
+         
+         /*if (filter.isEmpty()) {
+            System.out.println("Please enter at least one character.");
+         } else {*/
+            success = true;
+         //}
+      } while(!success);
       
-      return accessToken;
+      return filter;
    }
    
    /**
-    * Get and show Tweets, which are related to the given query.
-    * @param queryString the query's string
-    * @throws TwitterException 
-    */
-   public static void getAndShowTweets(String queryString) throws TwitterException {
-      Query query = new Query(queryString);
-      QueryResult result = twitter.search(query);
-
-      System.out.println(result.getTweets().size() + " available Tweet(s).");
-
-      result.getTweets().stream().forEach((status) -> {
-         System.out.println("@" + status.getUser().getScreenName() + ":" + status.getText());
-      });
-   }
-   
-   public static void readStreaming(Configuration cb, String queryString,
-                                    double[] southwestCoordinates, double[] northeastCoordinates) {
-      StatusListener listener = new StatusListener() {
-         @Override
-         public void onStatus(Status status) {
-            GeoLocation geoLocation = status.getGeoLocation();
-            
-            if (geoLocation != null) {
-               double longitude = geoLocation.getLongitude();
-               double latitude = geoLocation.getLatitude();
-               
-               if (longitude >= southwestCoordinates[0] && longitude <= northeastCoordinates[0] &&
-                   latitude >= southwestCoordinates[1] && latitude <= northeastCoordinates[1]) {
-                  System.out.println(longitude + "," + latitude + " => " + status.getUser().getName() + " : " + status.getText());
-               }
-            }
-         }
-         
-         @Override
-         public void onDeletionNotice(StatusDeletionNotice statusDeletionNotice) {
-            System.out.println("Got a status deletion notice id:" + statusDeletionNotice.getStatusId());
-         }
-         
-         @Override
-         public void onTrackLimitationNotice(int numberOfLimitedStatuses) {
-            System.out.println("Got track limitation notice:" + numberOfLimitedStatuses);
-         }
-
-         @Override
-         public void onScrubGeo(long userId, long upToStatusId) {
-            System.out.println("Got scrub_geo event userId:" + userId + " upToStatusId:" + upToStatusId);   
-         }
-
-         @Override
-         public void onStallWarning(StallWarning warning) {
-            System.out.println("Got stall warning:" + warning);
-         }
-         
-         @Override
-         public void onException(Exception ex) {
-            ex.printStackTrace();
-         }
-      };
-      
-      TwitterStream twitterStream = new TwitterStreamFactory(cb).getInstance();
-      twitterStream.addListener(listener);
-      FilterQuery fq = new FilterQuery(queryString);
-      twitterStream.filter(fq);
-   }
-   
-   /**
-    * Get the application's access token and get some Tweets with the REST/Streaming APIs.
     * @param args the command line arguments
     * @throws twitter4j.TwitterException
     * @throws java.io.IOException
+    * @throws java.lang.InterruptedException
+    * @throws java.util.concurrent.ExecutionException
     */
-   public static void main(String[] args) throws TwitterException, IOException {
-      AccessToken accessToken = getApplicationAccessToken();
+   public static void main(String[] args) throws TwitterException, IOException, InterruptedException, ExecutionException {
+      MainExecution mainExecution = new MainExecution();
       
-      ConfigurationBuilder cb = new ConfigurationBuilder();
-      cb.setDebugEnabled(true)
-              .setOAuthConsumerKey(CONSUMER_KEY)
-              .setOAuthConsumerSecret(CONSUMER_SECRET)
-              .setOAuthAccessToken(accessToken.getToken())
-              .setOAuthAccessTokenSecret(accessToken.getTokenSecret());
-      
-      //getAndShowTweets("yverdon");
+      //mainExecution.getAndShowTweets("yverdon");
       
       // Switzerland's coordinates, format {longitude, latitude}
-      //double[] southwestCoordinates = {5.956085,45.817851};
-      //double[] northeastCoordinates = {10.489254,47.808454};
+      double[] southwestCoordinates = {5.956085, 45.817851};
+      double[] northeastCoordinates = {10.489254, 47.808454};
       // USA's coordinates
-      double[] southwestCoordinates = {-124.411668, 24.957884};
-      double[] northeastCoordinates = {-66.888435, 49.001895};
+      //double[] southwestCoordinates = {-124.411668, 24.957884};
+      //double[] northeastCoordinates = {-66.888435, 49.001895};
       
-      readStreaming(cb.build(), "job", southwestCoordinates, northeastCoordinates);
+      mainExecution.readStreaming(readTags(), southwestCoordinates, northeastCoordinates, false);
    }
 }
