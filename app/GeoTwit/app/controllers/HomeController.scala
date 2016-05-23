1ad771a50d2f198bbd63c2c4b9e5bd3e9b7145ca
edri <mig.santamar@gmail.com>
2016-05-23 21:34:33 +0200

    Update index page, add Twitter4J library to the project and implement the Twitter's connection.

--- app/GeoTwit/app/controllers/HomeController.scala
+++ app/GeoTwit/app/controllers/HomeController.scala
@@ -1,15 +1,19 @@
 package controllers
 
 import javax.inject._
-import play.api._
+
 import play.api.mvc._
+import twitter4j.auth.RequestToken
+import twitter4j.{Twitter, TwitterException, TwitterFactory}
 
 /**
  * This controller creates an `Action` to handle HTTP requests to the
  * application's home page.
  */
 @Singleton
-class HomeController @Inject() extends Controller {
+class HomeController extends Controller {
+  private val CONSUMER_KEY = "InKH0FAxYqggrNLXGzxYIlRIP"
+  private val CONSUMER_SECRET = "RG8g7CbESAM9MRbeJYOCchPyPxwdMZkcH9x76UXJkr4Ik8QJqK"
 
   /**
    * Create an Action to render an HTML page with a welcome message.
@@ -17,8 +21,40 @@ class HomeController @Inject() extends Controller {
    * will be called when the application receives a `GET` request with
    * a path of `/`.
    */
-  def index = Action {
-    Ok(views.html.index())
+  def index(error: Option[String]) = Action {
+    Ok(views.html.index(error))
+  }
+
+  def auth = Action { request =>
+    val url =
+      if(request.host.contains(":9000"))
+        "http://" + request.host + "/callback"
+      else
+        "https://" + request.host + "/callback"
+
+    val twitter: Twitter = new TwitterFactory().getInstance()
+    twitter.setOAuthConsumer(CONSUMER_KEY, CONSUMER_SECRET)
+
+    try {
+      val requestToken: RequestToken = twitter.getOAuthRequestToken(url)
+      Redirect(requestToken.getAuthenticationURL)
+    } catch {
+      case e: TwitterException => Redirect(routes.HomeController.index(Some("errorGettingApplicationToken")))
+    }
+  }
+
+  def callback(denied: Option[String], oauthToken: Option[String], oauthVerifier: Option[String]) = Action {
+    denied match {
+      case Some(_) => Redirect(routes.HomeController.index(Some("errorDenied")))
+      case None => (oauthToken, oauthVerifier) match {
+        case (Some(_), Some(_)) => Redirect(routes.HomeController.search)
+        case _ => Redirect(routes.HomeController.index(Some("error")))
+      }
+    }
+  }
+
+  def search = Action {
+    Ok(views.html.search())
   }
 
 }
